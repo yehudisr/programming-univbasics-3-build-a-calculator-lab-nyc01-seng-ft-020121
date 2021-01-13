a61311d1c6e41e823eab69a6b848737264252d0a
yehudis raitman <yehudisr@gmail.com>
2021-01-13 22:17:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,16 +5,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-    sum = num1 - num1
+    sum = num1 - num2
     return sum
 end
 
 def division(num1, num2)
-
+    sum = num2/num1
+    return sum
 end
 
 def multiplication(num1, num2)
-
+    sum = num1*num2
+    return sum
 end
 
 def modulo(num1, num2)
